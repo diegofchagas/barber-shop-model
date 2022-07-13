f518d95b0a8a839a695deccdebcd530f36010483
Diego Chagas <diegofchagas@gmail.com>
2022-07-13 12:39:20 -0300

    alterando fotos da pagina

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const icone = document.getElementById("menu-hambuguer");
+
+icone.addEventListener("click",  function(){
+
+    const menu = document.getElementsByTagName("nav")[0]
+    menu.style.display = "flex";
+
+})
+
+
+
